203fb954f23403d7f8625767e360ed47d90e017e
Ayadav1998 <ayadav1998.ay@gmail.com>
2021-10-04 11:45:47 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,8 @@
+alert("This Website is Under Developing Phase !");
+
+  function myFunction() {
+    alert("I Told You System under maintenance!");
+  }
+  function myBtnFunction(){
+    alert("Your Message Submit Successfully, Thank You !");
+  }
